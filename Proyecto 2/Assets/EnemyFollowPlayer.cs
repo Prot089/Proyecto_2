8b5e46e0f369bafa303088c6439d9f296214fe63
Alejandro <zap561@amerike.edu.mx>
2022-10-03 11:14:32 -0500

    FollowMovement

---
+++ Proyecto 2/Assets/EnemyFollowPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    public float speed;
+    private Transform player;
+    public float lineOfSite;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    
+    void Update()
+    {
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if (distanceFromPlayer < lineOfSite)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    void onDragGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+    }
+}
