4640fad41820099f602826bbf7faefbb65a5b9ef
Alejandro <zap561@amerike.edu.mx>
2022-10-04 08:29:13 -0500

    Physics
    Idk

--- Proyecto 2/Assets/_Main/Assets/Scripts/PlayerMovement.cs
+++ Proyecto 2/Assets/_Main/Assets/Scripts/PlayerMovement.cs
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Fisicas
+        //Physics
         playerRB.MovePosition(playerRB.position + moveInput * speed * Time.fixedDeltaTime);
     }
 }
