3ec59bca525a4173a11f68c7f15801fe9006923b
Alejandro <zap561@amerike.edu.mx>
2022-10-10 08:07:02 -0500

    I erased libraries that werent used
    Hello partner, I did what you asked me to.

--- Proyecto 2/Assets/_Main/Assets/Scripts/ItemObject.cs
+++ Proyecto 2/Assets/_Main/Assets/Scripts/ItemObject.cs
@@ -12,7 +12,7 @@ public class ItemObject : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             OnHandlePickUp();
         }
